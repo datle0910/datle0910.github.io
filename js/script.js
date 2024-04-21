d8e921c207cd1a55dd48fa80eb0b86a0185ca8e1
datle0910 <va.09102003@gmail.com>
2024-04-21 17:09:55 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,82 +1,82 @@
 $(document).ready(function() {
     $("#DCP-T220").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "DCP-T220.html"; 
+        var destinationURL = "./html/DCP-T220.html"; 
         window.location.href = destinationURL;
     });
     $("#DCP-T720").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "DCP-T720DW.html"; 
+        var destinationURL = "./html/DCP-T720DW.html"; 
         window.location.href = destinationURL;
     });
     $("#B7535").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "B7535DW.html"; 
+        var destinationURL = "./html/B7535DW.html"; 
         window.location.href = destinationURL;
     });
     $("#MFP").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "MFP.html"; 
+        var destinationURL = "./html/MFP.html"; 
         window.location.href = destinationURL;
     });
     $("#L2520D").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "L2520D.html"; 
+        var destinationURL = "./html/L2520D.html"; 
         window.location.href = destinationURL;
     });
     $("#135a").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "135a.html"; 
+        var destinationURL = "./html/135a.html"; 
         window.location.href = destinationURL;
     });
     $("#T520W").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "T520W.html"; 
+        var destinationURL = "./html/T520W.html"; 
         window.location.href = destinationURL;
     });
     $("#LBP2900").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "LBP2900.html"; 
+        var destinationURL = "./html/LBP2900.html"; 
         window.location.href = destinationURL;
     });
     $("#C330").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "DS-C330.html"; 
+        var destinationURL = "./html/DS-C330.html"; 
         window.location.href = destinationURL;
     });
     $("#ADS3100").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "ADS3100.html"; 
+        var destinationURL = "./html/ADS3100.html"; 
         window.location.href = destinationURL;
     });
     $("#3600F1").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "3600F1.html"; 
+        var destinationURL = "./html/3600F1.html"; 
         window.location.href = destinationURL;
     });
     $("#2000S2").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "2000S2.html"; 
+        var destinationURL = "./html/2000S2.html"; 
         window.location.href = destinationURL;
     });
     $("#3000S4").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "3000S4.html"; 
+        var destinationURL = "./html/3000S4.html"; 
         window.location.href = destinationURL;
     });
     $("#5000S5").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "5000S5.html"; 
+        var destinationURL = "./html/5000S5.html"; 
         window.location.href = destinationURL;
     });
     $("#2600F1").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "2600F1.html"; 
+        var destinationURL = "./html/2600F1.html"; 
         window.location.href = destinationURL;
     });
     $("#N9120").click(function (e) { 
         e.preventDefault();
-        var destinationURL = "N9120.html"; 
+        var destinationURL = "./html/N9120.html"; 
         window.location.href = destinationURL;
     });
     
@@ -101,7 +101,7 @@ $(document).ready(function() {
                 resultHTML += '<div class="col">' + product.innerHTML + '</div>';
             });
             sessionStorage.setItem('searchResults', resultHTML);
-            window.location.href = '../BaiTapDoAn/html/KetQuaTimKiem.html';
+            window.location.href = './html/KetQuaTimKiem.html';
         } else {
             alert('Không tìm thấy sản phẩm nào phù hợp với từ khóa tìm kiếm.');
         }
